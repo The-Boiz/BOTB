cca283de0b5a921036ecb5bcbb497fa9547bce35
dspmanbaby <32938970+dspmanbaby@users.noreply.github.com>
2020-03-06 14:18:11 -0800

    User Verification

--- app/src/main/java/com/example/botb/SignUpActivity.java
+++ app/src/main/java/com/example/botb/SignUpActivity.java
@@ -9,7 +9,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_verification);
+        setContentView(R.layout.);
 
     }
 }
--- app/src/main/java/com/example/botb/UserVerificationActivity.java
+++ app/src/main/java/com/example/botb/UserVerificationActivity.java
@@ -1,28 +1,38 @@
 package com.example.botb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserVerificationActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button signIn;
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_verification);
-        EditText username = findViewById(R.id.editText3);
-        EditText password = findViewById(R.id.editText4);
-        Button signIn = findViewById(R.id.signInButton);
-        Button signUp = findViewById(R.id.signUpButton);
-        
+         username = findViewById(R.id.editText3);
+         password = findViewById(R.id.editText4);
+         signIn = findViewById(R.id.signInButton);
+         signUp = findViewById(R.id.signUpButton);
+
+
 
     }
 
+    public void signUpIntent(View v){
+        Intent intent = new Intent(this,SignUpActivity.class);
+        intent.putExtra("username",);
+        intent.putExtra("password", );
+        startActivity(intent);
 
-
-
-
+    }
 
 }
