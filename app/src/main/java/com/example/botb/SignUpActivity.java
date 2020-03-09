ae2a9269f3cb6c8192aa8a68b23d0d576f2a5881
dspmanbaby <32938970+dspmanbaby@users.noreply.github.com>
2020-03-09 14:26:11 -0700

    User Verification

--- app/src/main/java/com/example/botb/SignUpActivity.java
+++ app/src/main/java/com/example/botb/SignUpActivity.java
@@ -9,7 +9,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.user_signup);
 
     }
 }
